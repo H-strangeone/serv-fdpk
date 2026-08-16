51bd3d989067391641667dfe05bfd69e8288e062
H-strangeone <hiteshgitman@gmail.com>
2026-08-17 00:46:25 +0530

    formatting and playing with the idea

--- protocol/src/packet/payload.rs
+++ protocol/src/packet/payload.rs
@@ -2,6 +2,25 @@ use serde::{serialize, Deserialize};
 #[repr(u8)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum Payloadformat{
-    rawbinary =0x00;
-    
+    Rawbinary =0x00;
+    Json=0x01;
+    Messasgepack=0x02;
+    Bson=0x03;
+    Protobuf=0x04;
+    Custom=0xFF;
+
+}
+
+impl Payloadformat{
+    pub fn from_u8(byte: u8)-> Option<Self>{
+        match byte{
+            0x00 => Some(Payloadformat::Rawbinary),
+            0x01 => Some(Payloadformat::Json),
+            0x02 => Some(Payloadformat::Messasgepack),
+            0x03 => Some(Payloadformat::Bson),
+            0x04 => Some(Payloadformat::Protobuf),
+            0xFF => Some(Payloadformat::Custom),
+            _ => None,
+        }
+    }
 }
